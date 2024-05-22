7d3b3b05e8f8d07bb3191ad522d1602cf8898248
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-05-22 04:18:42 +0300

    Define event handlers on the input and button elements

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function changeHandler(){
+        console.log("Entering password...")
+    }
+    
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={changeHandler}></input>
+        </div>
     )
 }
 
